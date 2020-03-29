b6df8941c52bedaa5bc1f0dbac7d9e687a97a71b
Shivam Anand <anand.shivam44@yahoo.com>
2020-03-29 19:31:00 +0530

    ADDED

---
+++ app/src/main/java/com/example/effortmanagement_jh/Exampleadapter.java
@@ -0,0 +1,84 @@
+package com.example.effortmanagement_jh;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Exampleadapter extends RecyclerView.Adapter<Exampleadapter.ExampleViewHolder> {
+    private ArrayList<Example_item> mexampleList;
+    private onNoteListener monNoteListener;
+
+    public  class ExampleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ImageView mImageView;
+        public TextView mtextView1;
+        public TextView mtextView2;
+        onNoteListener onNoteListener;
+
+
+        public ExampleViewHolder(@NonNull View itemView, onNoteListener onNoteListener) {
+            super(itemView);
+            mImageView = itemView.findViewById(R.id.imageview);
+            mtextView1 = itemView.findViewById(R.id.textview1);
+            mtextView2 = itemView.findViewById(R.id.textview2);
+            this.onNoteListener = onNoteListener;
+
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            onNoteListener.onNoteListenre(getAdapterPosition());
+
+        }
+    }
+    public Exampleadapter(ArrayList<Example_item> exampleList, onNoteListener onNoteListener){
+        mexampleList = exampleList;
+
+
+
+
+        this.monNoteListener = onNoteListener;
+
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.example_item,parent,false);
+        ExampleViewHolder evh = new ExampleViewHolder(v , monNoteListener);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder holder, int position) {
+
+        Example_item currentitem = mexampleList.get(position);
+        holder.mImageView.setImageResource(currentitem.GetImageResource());
+        holder.mtextView1.setText(currentitem.getmText1());
+        holder.mtextView2.setText(currentitem.getmText2());
+        //holder.itemView.setOnClickListener();
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mexampleList.size();
+    }
+
+    //Inter face containing method......
+    public interface onNoteListener{
+        void onNoteListenre(int position);
+    }
+}
---
+++ app/src/main/java/com/example/effortmanagement_jh/Telephone.java
@@ -0,0 +1,65 @@
+package com.example.effortmanagement_jh;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Telephone extends AppCompatActivity implements Exampleadapter.onNoteListener {
+
+    ArrayList<Example_item> exampleList;
+
+    private RecyclerView mRecyclerview;
+    private RecyclerView.Adapter madapter;
+    private RecyclerView.LayoutManager mlayoutmanager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telephone);
+
+        exampleList = new ArrayList<>();
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Andhra Pradesh","0866-2410978"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Arunanchal Pradesh","9436055743"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Assam","6913347770"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Bihar","104"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Chhattisgarh","104"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Goa","104"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Gujrat","104"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Harayana","8558893911"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Jharkhand","104"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Himanchal Pradesh","104"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Karnataka","104"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Kerala","0471-2552056"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Madhya Pradesh","0755-2527177"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Maharashtra","020-26127394"));
+        exampleList.add(new Example_item(R.drawable.ic_call_black_phone,"Manipur","3852411668"));
+        mRecyclerview = findViewById(R.id.recyl);
+        mRecyclerview.setHasFixedSize(true);
+
+        mlayoutmanager = new LinearLayoutManager(this);
+        madapter = new Exampleadapter(exampleList,this);
+        mRecyclerview.setLayoutManager(mlayoutmanager);
+        mRecyclerview.setAdapter(madapter);
+    }
+    public void helo(View v){
+
+    }
+
+    @Override
+    public void onNoteListenre(int position) {
+        exampleList.get(position);
+        Example_item listitemok = exampleList.get(position);
+        String phoneno =listitemok.getmText2();
+        //Toast.makeText(getApplicationContext(),phoneno,Toast.LENGTH_LONG).show();
+        Uri u = Uri.parse("tel:"+phoneno);
+        Intent i = new Intent(Intent.ACTION_DIAL, u);
+        startActivity(i);
+    }
+}
